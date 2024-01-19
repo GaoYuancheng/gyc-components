7b490889c18ed328603a1f250ed3f3374d0e2754
gaoyuancheng <gaoyuancheng@pinming.cn>
2024-01-19 17:55:14 +0800

    fix: index export

---
+++ src/hooks/tool2/index.ts
@@ -0,0 +1,5 @@
+function tool2(obj: any) {
+  console.log('->', obj?.sex);
+}
+
+export default tool2;
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 // components
-// export { default as ClickWrap } from './components/ClickWrap';
+export { default as ClickWrap } from './components/ClickWrap';
 export { default as Foo } from './components/Foo';
-// export { default as GModal } from './components/GModal';
+export { default as GModal } from './components/GModal';
 export { default as GSelect } from './components/GSelect';
 
 // // hooks & tools
 export { default as tool1 } from './hooks/tool1';
+export { default as tool2 } from './hooks/tool2';
 // export { default as useHooks1 } from './hooks/useHook1';
